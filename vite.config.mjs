f9908b02f03d60435a0f52fa33d583c5482b5089
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-02-14 16:50:16 -0800

    Starts postcss with autoprefixer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import twig from 'vite-plugin-twig-drupal';
+import autoprefixer from 'autoprefixer';
 import { join } from "node:path"
 export default defineConfig({
   plugins: [
