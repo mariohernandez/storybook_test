3a99a3a5f19600f6f78da8550d1a7fd8b03174f7
Chaz Chumley <chaz.chumley@gmail.com>
2024-02-18 17:21:33 -0500

    configure postcss

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 import twig from 'vite-plugin-twig-drupal';
 import yml from '@modyfi/vite-plugin-yaml';
 import { join } from "node:path"
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
   root: 'src',
@@ -16,5 +17,15 @@ export default defineConfig({
       },
     }),
     yml(),
+    copy({
+      targets: [{ src: 'src/**/**.css', dest: 'dist/css' }],
+      hook: 'writeBundle',
+    }),
   ],
+  css: {
+    src: 'src/**/**',
+    postcss: {
+      config: './postcss.config.js'
+    }
+  }
 })
