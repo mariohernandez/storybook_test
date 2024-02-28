703f94e18843409a1dd9f298eff24854bcd2eb52
Chaz Chumley <chaz.chumley@gmail.com>
2024-02-28 14:24:12 -0500

    update button

--- components/component/button/button.stories.jsx
+++ components/component/button/button.stories.jsx
@@ -1,6 +1,5 @@
 import parse from 'html-react-parser';
 
-import { withGlobalWrapper } from '../../../.storybook/decorators';
 import button from './button.twig';
 import buttonData from './button.yml';
 import './button.css';
@@ -8,7 +7,6 @@ import './button.css';
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: 'Components/Button',
-  decorators: [withGlobalWrapper],
   tags: ['autodocs'],
 };
 
